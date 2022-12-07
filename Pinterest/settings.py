4581a6293cec5981b5d858bca304108e63e63bfb
DESKTOP-O166TQ7\DP <jaeh@d-platform.co.kr>
2022-12-07 16:14:33 +0900

    second commit

--- Pinterest/settings.py
+++ Pinterest/settings.py
@@ -23,7 +23,7 @@ env = environ.Env(
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 environ.Env.read_env(
-    env_file = os.path.join(BASE_DIR, '.env')
+    env_file=os.path.join(BASE_DIR, '.env')
 )
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
