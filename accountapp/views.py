72e24c84d14896c048ada15ec4f8e218f91fced0
DESKTOP-O166TQ7\DP <jaeh@d-platform.co.kr>
2022-12-08 12:13:08 +0900

    django course 22 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -31,4 +31,3 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
-    # return HttpResponse("Hello world!")
