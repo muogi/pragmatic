c7349e3c21504beca4d0967bbd83cbc9419470ef
DESKTOP-O166TQ7\DP <jaeh@d-platform.co.kr>
2023-01-05 11:51:33 +0900

    mysqldb로 변경

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -89,10 +89,21 @@ WSGI_APPLICATION = "pragmatic.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "pinterestdb",
+        "USER": "root",
+        "PASSWORD": "1q2w3e4r",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
